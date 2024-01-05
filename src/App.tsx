173a22346f78e66cc1e4c9854a7fa327088022f2
Maks Thimbarov <thimbarov@gmail.com>
2024-01-05 19:01:34 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+ return (
+  <div>
+   <p className="text-blue-600">Welcome to Luna Edge technical interview</p>
+  </div>
+ );
+}
+
+export default App;
